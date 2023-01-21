9bdfdd99b254764f6913596bd0f6a08586d0b1dc
grizzlyq <pechenika73@yandex.ru>
2023-01-21 22:50:55 +0300

    Initial Commit

---
+++ SliderScore_SwiftUI/SliderScore_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderScore_SwiftUIApp.swift
+//  SliderScore_SwiftUI
+//
+//  Created by Дмитрий on 21.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderScore_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
